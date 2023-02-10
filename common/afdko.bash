4ca4334469c8035b137f1257fbfb6ce9b12fc80d
msizanoen1 <msizanoen@qtmlabs.xyz>
2023-02-10 19:48:51 +0700

    common/afdko: use python.bash

--- common/afdko.bash
+++ common/afdko.bash
@@ -1,14 +1,11 @@
-afdko="$(realpath "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/afdko)"
+afdko="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/afdko
 
-if ! command -v python3 >/dev/null; then
-        echo "Error: python3 is required"
-        exit 1
-fi
+source "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/python.bash
 
 if ! [ -f "$afdko"/installed ]; then
         echo "Installing AFDKO..."
         rm -rf "$afdko"
-        python3 -m venv "$afdko"
+        "$python" -m venv "$afdko"
         "$afdko"/bin/pip install -U pip setuptools wheel
         "$afdko"/bin/pip install afdko
         touch "$afdko"/installed
